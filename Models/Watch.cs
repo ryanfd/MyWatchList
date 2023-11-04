67b3d06dcd8df9f35e56e5d48a66950b4c151221
Ryan <rdonnelly98@gmail.com>
2023-11-04 10:48:55 -0700

    initial environment

---
+++ Models/Watch.cs
@@ -0,0 +1,16 @@
+﻿namespace MyWatchListWebApp.Models
+{
+    public class Watch
+    {
+        public string RefNum { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Movement { get; set; }
+        public string CaseMaterial { get; set; }
+        public string BandMaterial { get; set; }
+        public double PowerReserve { get; set; }
+        public double CaseDiameter { get; set; }
+
+
+    }
+}
